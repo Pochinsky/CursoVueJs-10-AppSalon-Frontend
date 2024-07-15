0abeac1f80863422c06a6b1711316e11e5469c0b
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-07-15 18:10:31 -0400

    🎉 project initializated

---
+++ src/views/appointments/AppointmentsLayout.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div class="flex justify-between">
+    <h1 class="text-2xl lg:text-6xl font-black text-white">App Salón</h1>
+    <div class="flex flex-col space-y-5">
+      <div class="flex gap-2 items-center">
+        <p class="text-white text-right">Hola Usuario</p>
+        <button
+          class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+        >
+          Cerrar Sesión
+        </button>
+      </div>
+      <nav class="flex gap-2 items-center justify-end">
+        <button
+          class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg hover:text-gray-300"
+        >
+          Mis Citas
+        </button>
+        <RouterLink
+          class="py-2 px-5 text-gray-200 uppercase text-xs font-extrabold rounded-lg bg-blue-600 hover:bg-blue-700"
+          :to="{ name: 'new-appointment' }"
+        >
+          Nueva Cita
+        </RouterLink>
+      </nav>
+    </div>
+  </div>
+  <main>
+    <RouterView />
+  </main>
+</template>
