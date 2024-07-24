9a0f882cf2edfe31df7be77c16f2e33cf693fa6b
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-07-24 17:46:15 -0400

    ✨ logout added

--- src/views/appointments/AppointmentsLayout.vue
+++ src/views/appointments/AppointmentsLayout.vue
@@ -1,13 +1,18 @@
-<script setup></script>
+<script setup>
+import { usUserStore } from "@/stores/user";
+
+const userStore = usUserStore();
+</script>
 
 <template>
   <div class="flex justify-between">
     <h1 class="text-2xl lg:text-6xl font-black text-white">App Salón</h1>
     <div class="flex flex-col space-y-5">
       <div class="flex gap-2 items-center">
-        <p class="text-white text-right">Hola Usuario</p>
+        <p class="text-white text-right">Hola {{ userStore.getUserName }}</p>
         <button
           class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+          @click="userStore.logout"
         >
           Cerrar Sesión
         </button>
