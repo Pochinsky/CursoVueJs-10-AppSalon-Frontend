e0524d4e24f54046d5f196b3949b1c45955f1a24
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-07-30 09:55:42 -0400

    ✨ admin panel added

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: "/admin",
+      name: "admin",
+      component: () => import("../views/admin/AdminLayout.vue"),
+      meta: { requiresAdmin: true },
+      children: [
+        {
+          path: "",
+          name: "admin-appointments",
+          component: () => import("../views/admin/AppointmentsView.vue"),
+        },
+      ],
+    },
+    {
       path: "/reservaciones",
       name: "appointments",
       component: AppointmentsLayout,
@@ -100,14 +113,25 @@ router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.matched.some((url) => url.meta.requiresAuth);
   if (requiresAuth) {
     try {
-      await AuthAPI.auth();
+      const { data } = await AuthAPI.auth();
+      if (data.admin) next({ name: "admin" });
+      else next();
+    } catch (error) {
+      next({ name: "login" });
+    }
+  } else next();
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAdmin = to.matched.some((url) => url.meta.requiresAdmin);
+  if (requiresAdmin) {
+    try {
+      await AuthAPI.admin();
       next();
     } catch (error) {
       next({ name: "login" });
     }
-  } else {
-    next();
-  }
+  } else next();
 });
 
 export default router;
---
+++ src/views/admin/AppointmentsView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AdminAppointment from "@/components/AdminAppointment.vue";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+</script>
+
+<template>
+  <p class="text-white text-lg mt-5">
+    A continuación puedes administrar tus próximas citas
+  </p>
+  <p v-if="userStore.loading" class="text-white text-2xl text-center mt-5">
+    Cargando...
+  </p>
+  <div v-else>
+    <p
+      v-if="userStore.noAppointments"
+      class="text-white text-2xl text-center mt-5"
+    >
+      No tienes próximas citas
+    </p>
+    <div v-else class="grid grid-cols-1 gap-5 mt-10">
+      <AdminAppointment
+        v-for="appointment in userStore.userAppointments"
+        :key="appointment._id"
+        :appointment="appointment"
+      />
+    </div>
+  </div>
+</template>
